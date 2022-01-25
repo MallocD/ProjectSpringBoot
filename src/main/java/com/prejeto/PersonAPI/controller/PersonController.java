b2319924471da23a86cf719869c21cf3434e470d
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-01-25 09:38:52 -0400

    Finish

--- src/main/java/com/prejeto/PersonAPI/controller/PersonController.java
+++ src/main/java/com/prejeto/PersonAPI/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.prejeto.PersonAPI.dto.MessageResponseDTO;
 import com.prejeto.PersonAPI.dto.request.PersonDTO;
 import com.prejeto.PersonAPI.exception.PersonNotFoundException;
 import com.prejeto.PersonAPI.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +14,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")//Mapeamento da localidade
+@AllArgsConstructor(onConstructor = @__(@Autowired) )
 public class PersonController {
 
     private PersonService personService;
 
-    @Autowired
-    public PersonController(PersonService personService){
-        this.personService = personService;
-    }
 
     @PostMapping//Criação de novas pessoas
     @ResponseStatus(HttpStatus.CREATED)
--- src/main/java/com/prejeto/PersonAPI/service/PersonService.java
+++ src/main/java/com/prejeto/PersonAPI/service/PersonService.java
@@ -6,6 +6,7 @@ import com.prejeto.PersonAPI.entity.Person;
 import com.prejeto.PersonAPI.exception.PersonNotFoundException;
 import com.prejeto.PersonAPI.mapper.PersonMapper;
 import com.prejeto.PersonAPI.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +15,11 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired) )
 public class PersonService {
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
